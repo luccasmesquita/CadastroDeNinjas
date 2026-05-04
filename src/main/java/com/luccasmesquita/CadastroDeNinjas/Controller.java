df4cef1d600096a2ee51883ccc9df3ddc9c72052
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2026-05-04 22:09:46 +0100

    Criado a Controller

---
+++ src/main/java/com/luccasmesquita/CadastroDeNinjas/Controller.java
@@ -0,0 +1,17 @@
+package com.luccasmesquita.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //falando pro java que esse arquivo é um controller
+@RequestMapping //mapeando rotas
+public class Controller {
+    //controller vai mapear esse arquivo do java
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa é minha primeisa mensagem nessa rota";
+    }
+
+}
