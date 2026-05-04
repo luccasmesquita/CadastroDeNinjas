c07891ed872e486b76609f813dd443e71ab8cb38
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2026-05-04 14:25:31 +0100

    first commit

---
+++ src/test/java/com/luccasmesquita/CadastroDeNinjas/CadastroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luccasmesquita.CadastroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
